b02c5a34ce959091f7b75567d813c6233b38db4a
ANDREW MONTANO <amontano59@cnm.edu>
2022-01-31 17:20:12 -0700

    Add project files.

---
+++ MontanoQ1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Name: Andrew Montano
+//Email: amontano59@cnm.edu
+//File name: MontanoQ1
+
+using System;
+
+namespace MontanoQ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
