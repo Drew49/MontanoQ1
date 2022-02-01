bd7cf66f1f9f3964fccd931185b69b0dd5130264
ANDREW MONTANO <amontano59@cnm.edu>
2022-01-31 18:03:37 -0700

    Code written, now will test to see if it runs.

---
+++ MontanoQ1/Force.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontanoQ1
+{
+    internal class Force
+    {
+        //Fields
+
+        private string obj1;
+        private string obj2;
+        private double massObj1;
+        private double massObj2;
+        private double distanceBewteenObjs;
+        private double force;
+        private const double gravConst = (6.67408e-11);
+
+        //Accessors
+
+        public string GetObj1()
+        {
+            return obj1;
+        }
+        public string GetObj2()
+        {
+            return obj2;
+        }
+        public double GetMassObj1()
+        {
+            return massObj1;
+        }
+        public double GetMassObj2()
+        {
+            return massObj2;
+        }
+        public double GetDistanceBewteenObjs()
+        {
+            return distanceBewteenObjs;
+        }
+        public double GetForce()
+        {
+            return force;
+        }
+        public string GetForceSummary()
+        {
+            return "The force between " + obj1 + " and " + obj2 + " is " + force + " newtonw.";
+        }
+        public void ForceCalc()
+        {
+            force = gravConst * ((massObj1 * massObj2) / (distanceBewteenObjs * distanceBewteenObjs));
+        }
+
+        //Mutators
+
+        public void SetObj1(string obj1)
+        {
+            this.obj1 = obj1;
+        }
+        public void SetObj2(string obj2)
+        {
+            this.obj2 = obj2;
+        }
+        public void SetMassObj1( double massObj1)
+        {
+            this.massObj1 = massObj1;
+            ForceCalc();
+        }
+        public void SetMassObj2(double massObj2)
+        {
+            this.massObj2 = massObj2;
+            ForceCalc();
+        }
+        public void SetDistanceBetweenObjs(double distanceBewteenObjs)
+        {
+            this.distanceBewteenObjs = distanceBewteenObjs;
+            ForceCalc();
+        }
+        
+
+        //Constructor
+        public Force()
+        {
+
+        }
+         
+    }
+}
--- MontanoQ1/Program.cs
+++ MontanoQ1/Program.cs
@@ -3,14 +3,43 @@
 //File name: MontanoQ1
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MontanoQ1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string doAnother = "";
+            do
+            {
+                Force st = new Force();
+                string obj1 = GetInput("What is the name of the first obj?: ");
+                st.SetObj1(obj1);
+                string obj2 = GetInput("What is the name of the second obj?: ");
+                st.SetObj2(obj2);
+                double massObj1 = double.Parse(GetInput("What is the mass of obj1 in kilograms?: "));
+                st.SetMassObj1(massObj1);
+                double massObj2 = double.Parse(GetInput("What is the mass of obj2 in kilograms?: "));
+                st.SetMassObj1(massObj1);
+                double distanceBewtweenObjs = double.Parse("What is the distance between the objects in meters?: ");
+                st.SetDistanceBetweenObjs(distanceBewtweenObjs);
+
+                Console.WriteLine(st.GetForceSummary());
+
+                Console.WriteLine("Try two different objects? y/n: ");
+                doAnother = Console.ReadLine();
+            } while (doAnother == "y");   
+        }
+
+        private static string GetInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
         }
     }
 }
